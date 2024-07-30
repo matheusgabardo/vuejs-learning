a65f1ee1f755586e69210fcef8cc836175354a71
Matheus Messias <matheusgabardo677@gmail.com>
2024-07-30 16:29:35 -0300

    reseted app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <div>
     
-    <div>
-      <h2>todos em aberto</h2>
-      <div v-for="todo in uncompletedTodos" :key="todo.id">
-        {{ todo.title }}
-        <input type="checkbox" name="" id="" v-model="todo.completed" />
-      </div>
-    </div>
-    <div>
-      <h2>todos completas</h2>
-      <div v-for="todo in completedTodos" :key="todo.id">
-          {{ todo.title }}
-          <input type="checkbox" name="" id="" v-model="todo.completed" />
-      </div>
-    </div>
   </div>
 </template>
 
@@ -23,52 +9,12 @@
     name: 'App',
     data() {
       return {
-        todos: [
-          {
-            "userId": 1,
-            "id": 1,
-            "title": "delectus aut autem",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 2,
-            "title": "quis ut nam facilis et officia qui",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 3,
-            "title": "fugiat veniam minus",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 4,
-            "title": "et porro tempora",
-            "completed": true
-          },
-          {
-            "userId": 1,
-            "id": 5,
-            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            "completed": false
-          }
-        ]
+        
       }
     },
     methods: {
-      onClick($evt){
-        console.log('click', $evt);
-      }
     },
     computed: {
-      uncompletedTodos(){
-        return this.todos.filter(todo => !todo.completed)
-      },
-      completedTodos(){
-        return this.todos.filter(todo => todo.completed)
-      }
     }
   }
 </script>
