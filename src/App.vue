27d0a4c9da7c1718f7d48614e6de913548186ba4
Matheus Messias <matheusgabardo677@gmail.com>
2024-08-08 13:28:55 -0300

    until video lesson 29 - added components, tailwind and app base

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="px-3 py-10 md:px-10 bg-gray-800">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoForm />
+      <TodoSpinner />
+      <TodoItems />
+      <TodoNotFound />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+<script>
+import TodoSpinner from '@/components/TodoSpinner'
+import TodoForm from './components/TodoForm.vue';
+import TodoItems from './components/TodoItems.vue';
+import TodoNotFound from './components/TodoNotFound.vue';
+export default {
+  name: 'App',
+  components: {
+    TodoSpinner,
+    TodoForm,
+    TodoItems,
+    TodoNotFound
   }
 }
-</style>
+</script>
+
+<style scoped></style>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="space-y-2">
+        <TodoItem />
+    </div>
+</template>
+
+<script setup>
+import TodoItem from './TodoItem.vue';
+
+
+</script>
+
+<style scoped></style>
